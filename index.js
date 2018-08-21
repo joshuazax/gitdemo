60351d83a66af0f4a70aed4e0369e066fd4442d4
lakiran <651683826@qq.com>
2018-08-21 11:14:19 +0800

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+let oDiv = document.getElementById('demo');
+oDiv.onclick = function(){
+    this.style.width = '200px';
+    this.style.height = '200px';
+    this.style.backgroundColor = 'red';
+    this.style.lineHeight = '200px';
+    this.innerText = 'emmmmmmm';
+}
